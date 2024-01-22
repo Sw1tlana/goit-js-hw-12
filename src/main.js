8642fe025db1321f8f8bc2a249559beb82dd2587
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-01-22 13:27:21 +0200

    changes main.js

--- src/main.js
+++ src/main.js
@@ -10,10 +10,9 @@ const galleryImages = document.querySelector("ul.gallery");
 const loader = document.querySelector(".loader");
 const loadBtn = document.querySelector(".load-btn");
 
-let perPage = 20;
+let perPage = 40;
 let page = 1;
 let currentQuery = "";
-let totalHits = 0;
 
 searchForm.addEventListener("submit", searchFormSubmit);
 loadBtn.addEventListener("click", loadMoreImages);
@@ -31,61 +30,49 @@ async function searchFormSubmit(evt) {
         return;
     }
 
-   page = 1; 
-   currentQuery = query;
-
-        try {
-            const images = await fetchImages(currentQuery);
-            renderImages(images);
-            
-
-        } catch (error) {
-            console.log(error);
-            loader.style.display = "none";
-            form.reset();
-        }
-    }
-
-    async function loadMoreImages() {
-      try {
-        const images = await fetchImages(currentQuery);
-        renderImages(images);
-     
-        page += 1;
+    page = 1; 
+    currentQuery = query;
     
-        if (page * perPage >= totalHits) {
-          loadBtn.style.display = "none";
-          iziToast.show({
-            message: '✅ All images have been loaded!',
-            messageColor: '#ffffff',
-            backgroundColor: '#41B883',
-            position: 'topRight',
-            maxWidth: '420px',
-            close: false,
-          });
-        } else {
-            loadBtn.style.display = "block";
-        }
-      } catch (error) {
-        console.log(error);
-        loadBtn.style.display = "none";
-      }
-      
+    try {
+        await loadMoreImages();
+    } catch (error) {
+        console.error(error);
+        loader.style.display = "none";
+        form.reset();
     }
+}
 
-    async function fetchImages(currentQuery) {
-        const params = new URLSearchParams({
-            q: currentQuery,
-            image_type: "photo",
-            orientation: "horizontal",
-            safesearch: true,
-            _limit: perPage,
-            _page: page,
-        })
+async function loadMoreImages() {
+    try {
+        const images = await fetchImages(currentQuery);       
+            renderImages(images);
+            page += 1;
+            if (page > 1 ) {
+                loadBtn.style.display = "none";
+            } else {
+                loadBtn.style.display = "block";
+            }
+        
+    } catch (error) {
+        console.error(error);
+        loadBtn.style.display = "none";
+        showErrorNotification("Sorry, an error occurred. Please try again.");
+    }
+}
 
-        try {
-            const response = await axios.get(`https://pixabay.com/api/?key=41802498-7aef04e1b4b4791f33c618bc1&q=${currentQuery}&${params}`);
-            loadBtn.style.display = "block";
+async function fetchImages(currentQuery) {
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=41802498-7aef04e1b4b4791f33c618bc1`, {
+            params: {
+                q: currentQuery,
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: true,
+                per_page: perPage,
+                page: page,
+            }
+        });
+        loadBtn.style.display = "block";
             if (!response.data.total) {
                 iziToast.show({
                     message: '❌Sorry, there are no images matching your search query. Please try again!',
@@ -95,41 +82,60 @@ async function searchFormSubmit(evt) {
                     maxWidth: '420px',
                     close: false,
                 });
-                loadBtn.style.display = "none";
-                galleryImages.innerHTML = "";
-            }
-    
-            return response.data.hits;
+                
+        galleryImages.innerHTML = "";
+    }
+        return response.data.hits;
 
-        } catch (error) {
-            console.log(error);
-            loader.style.display = "none";
-        }   
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
-function renderImages(images) { 
-const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-    return `
- <li class="gallery-item">
- <a class="link-gallery" href="${largeImageURL}">
- <img class="gallery-images" src="${webformatURL}" alt="${tags}">
- </a>
- <div class="photo-info">
-     <p class="info-number"><strong>Like</strong> ${likes}</p>
-     <p class="info-number"><strong>Views</strong> ${views}</p>
-     <p class="info-number"><strong>Comments</strong> ${comments}</p>
-     <p class="info-number"><strong>Downloads</strong> ${downloads}</p> 
- </div>
- </li>
-`
-}).join("");
+function renderImages(images) {
+    const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+            <li class="gallery-item">
+                <a class="link-gallery" href="${largeImageURL}">
+                    <img class="gallery-images" src="${webformatURL}" alt="${tags}">
+                </a>
+                <div class="photo-info">
+                    <p class="info-number"><strong>Like</strong> ${likes}</p>
+                    <p class="info-number"><strong>Views</strong> ${views}</p>
+                    <p class="info-number"><strong>Comments</strong> ${comments}</p>
+                    <p class="info-number"><strong>Downloads</strong> ${downloads}</p>
+                </div>
+            </li>
+        `;
+    }).join("");
 
-galleryImages.insertAdjacentHTML("beforeend", markup);
-    
-const lightbox = new SimpleLightbox(document.querySelectorAll('.gallery a'));
+    galleryImages.insertAdjacentHTML("beforeend", markup);
 
-lightbox.refresh();
+    const lightbox = new SimpleLightbox(document.querySelectorAll('.gallery a'));
+    // const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
 }
 
+function showErrorNotification(message) {
+    iziToast.show({
+        message: `❌ ${message}`,
+        messageColor: '#ffffff',
+        backgroundColor: '#EF4040',
+        position: 'topRight',
+        maxWidth: '420px',
+        close: false,
+    });
+}
 
+// function showInfoNotification(message) {
+//     iziToast.show({
+//         message: `✅ ${message}`,
+//         messageColor: '#ffffff',
+//         backgroundColor: '#41B883',
+//         position: 'topRight',
+//         maxWidth: '420px',
+//         close: false,
+//     });
+// }
 
